cedcc177a8e0222e950cc8788f94ba4687dd4f09
Martin Gaens <djsushi123@gmail.com>
2023-02-04 16:49:29 +0100

    Remove useless modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';
-import { BoardComponent } from './board/board.component';
-import { SquareComponent } from './square/square.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    XyzComponent,
-    BoardComponent,
-    SquareComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

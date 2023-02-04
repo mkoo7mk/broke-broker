035a657dc94a1d962fcb8cd9f07075cf759b63de
Martin Gaens <djsushi123@gmail.com>
2023-02-04 17:24:16 +0100

    Create a reset SCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';
-<<<<<<< HEAD
-=======
-import { BoardComponent } from './board/board.component';
-import { SquareComponent } from './square/square.component';
-import { NavbarComponent } from './navbar/navbar.component';
->>>>>>> 65860c6 (Navbar start)
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    XyzComponent
-=======
-    XyzComponent,
-    BoardComponent,
-    SquareComponent,
     NavbarComponent
->>>>>>> 65860c6 (Navbar start)
   ],
   imports: [
     BrowserModule,
